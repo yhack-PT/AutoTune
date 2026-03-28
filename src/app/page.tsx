207a4f13f8e6c6c3db07f400b06940c0b1df1f16
“RichardBuild” <richoh123456@gmail.com>
2026-03-28 14:04:48 -0400

    got rid of dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
 import { ModelConfiguration } from "@/components/model-configuration";
-import { ThemeToggle } from "@/components/theme-toggle";
 
 export default function Home() {
   return (
-    <div className="relative flex min-h-screen items-center justify-center bg-background p-6 transition-colors md:p-8">
-      <div className="fixed top-4 right-4 z-10 md:top-6 md:right-6">
-        <ThemeToggle />
-      </div>
+    <div className="flex min-h-screen items-center justify-center bg-background p-6 md:p-8">
       <ModelConfiguration />
     </div>
   );
